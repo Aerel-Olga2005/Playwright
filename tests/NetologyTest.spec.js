b167b57a76c11ec072fe72419a805549ba9e4c0d
Helga NN <o9351030@gmail.com>
2026-08-31 20:28:03 +0300

    Initial commit

---
+++ tests/NetologyTest.spec.js
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+const { chromium } = require("playwright");
+const { email, password } = require("../user");
+
+test("test positive", async () => {
+  const browser = await chromium.launch({ headless: false });
+
+  const page = await browser.newPage();
+  await page.goto("https://netology.ru/?modal=sign_in");
+
+  await page.getByText('Другие способы входа').click();
+  await page.getByText('Войти по почте').click();
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole("textbox", { name: "Email" }).fill(user.email);
+  await page.getByRole('textbox', { name: 'Пароль' }).click();  
+  await page.getByRole("textbox", { name: "Пароль" }).fill(user.password);
+  await page.getByTestId('login-submit-btn').click();
+
+  await expect(page.getByText("Здравствуйте, Ольга!")).toBeVisible();
+
+  await browser.close();
+});
+
+test("test negative", async () => {
+  const browser = await chromium.launch({ headless: false });
+
+  const page = await browser.newPage();
+  await page.goto("https://netology.ru/?modal=sign_in");
+
+  await page.getByText('Другие способы входа').click();
+  await page.getByText('Войти по почте').click();
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('ftuyfugu');
+  await page.getByRole('textbox', { name: 'Пароль' }).click();
+
+  await expect(page.getByText("Неверный email")).toBeVisible();
+
+  await browser.close();
+});
