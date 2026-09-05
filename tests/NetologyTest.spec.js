52ce7c79a16b2e83df019b884373b36064ab99d8
Helga NN <o9351030@gmail.com>
2026-09-05 19:51:26 +0300

    user

--- tests/NetologyTest.spec.js
+++ tests/NetologyTest.spec.js
@@ -11,12 +11,12 @@ test("test positive", async () => {
   await page.getByText('Другие способы входа').click();
   await page.getByText('Войти по почте').click();
   await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole("textbox", { name: "Email" }).fill(user.email);
+  await page.getByRole("textbox", { name: "Email" }).fill(email);
   await page.getByRole('textbox', { name: 'Пароль' }).click();  
-  await page.getByRole("textbox", { name: "Пароль" }).fill(user.password);
+  await page.getByRole("textbox", { name: "Пароль" }).fill(password);
   await page.getByTestId('login-submit-btn').click();
 
-  await expect(page.getByText("Здравствуйте, Ольга!")).toBeVisible();
+  await expect(page.getByText("Учим - и помогаем на каждом этапе")).toBeVisible();
 
   await browser.close();
 });
